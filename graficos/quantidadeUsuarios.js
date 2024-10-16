5fc94713d5014271228fd1416c536b40bcfcc78b
natnoveli <128822111+natnoveli@users.noreply.github.com>
2024-10-16 08:03:54 -0300

    aula 10

--- graficos/quantidadeUsuarios.js
+++ graficos/quantidadeUsuarios.js
@@ -18,10 +18,30 @@ async function quantidadeUsuarios() {
     }
   ]
 
-  const layout = {
-    plot_bgcolor: getCSS('--bg-color'),
-    paper_bgcolor: getCSS('--bg-color')
-  }
+  
+
+const layout = {
+  plot_bgcolor: getCSS('--bg-color'),
+  paper_bgcolor: getCSS('--bg-color'),
+  title: {
+    text: 'Redes sociais com mais usuários no mundo',
+     x: 0,
+     font: {
+         color: getCSS('--primary-color'),
+         family: getCSS('--font'),
+         size: 30
+     }
+  },
+    xaxis: {
+    
+    },
+    yaxis: {
+    
+    }
+}
+
+
+
 
   const grafico = document.createElement('div')
   grafico.className = 'grafico'
